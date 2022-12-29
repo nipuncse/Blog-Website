070d204e414f631b657d15857ac033d35917444f
nipuncse <72141162+nipuncse@users.noreply.github.com>
2022-12-30 01:07:30 +0530

    initiated backend

--- clientside/src/Components/Sidebar1.js
+++ clientside/src/Components/Sidebar1.js
@@ -1,6 +1,11 @@
 import { Sidebar, Menu, MenuItem, useProSidebar, SubMenu } from 'react-pro-sidebar';
 import { VscChromeClose, VscNoNewline } from "react-icons/vsc";
 import { SlMenu } from "react-icons/sl";
+import { BiCategory } from "react-icons/bi";
+import { BsPersonFill } from "react-icons/bs";
+import { HiAnnotation } from "react-icons/hi";
+import { RxExit } from "react-icons/rx";
+
 import classes from '../css/sidebar.module.css'
 
 
@@ -9,8 +14,9 @@ export default function Sidebar1() {
 
 	return (
 		<div style={{ display: 'flex', height: '100vh' }}>
-			<Sidebar backgroundColor='rgb(69 193 247)' width='15vw' collapsedWidth='2vw' className={classes.mysidebar}>
-				<Menu menuItemStyles={{
+			<Sidebar backgroundColor='rgb(69 193 247)'
+				width='20vw' collapsedWidth='2vw' className={classes.mysidebar}>
+				<Menu style={{ marginTop: '25vh' }} menuItemStyles={{
 					button: ({ level, active, disabled }) => {
 						// only apply styles on first level elements of the tree
 						if (level === 0)
@@ -23,24 +29,26 @@ export default function Sidebar1() {
 				}}>
 					<MenuItem> Home </MenuItem>
 					<SubMenu label="Categories">
-						<MenuItem className={classes.mymenuitem}> Polity </MenuItem>
+
+						<MenuItem className={classes.mymenuitem}> Indian Polity </MenuItem>
 						<MenuItem>Geopolitics </MenuItem>
 						<MenuItem>Indian History </MenuItem>
 						<MenuItem>Economy </MenuItem>
 						<MenuItem>Geography </MenuItem>
 						<MenuItem>Politics </MenuItem>
+						<MenuItem>Tech </MenuItem>
 						<MenuItem>Personalities </MenuItem>
 					</SubMenu>
 
-					<MenuItem> Top Blogs </MenuItem>
-					<MenuItem> Top Authors</MenuItem>
-					<MenuItem> Top Categories</MenuItem>
-					<MenuItem> </MenuItem>
+					<MenuItem> Top Blogs    <HiAnnotation size='10%' /></MenuItem>
+					<MenuItem>Top Authors   <BsPersonFill size='10%' /></MenuItem>
+					<MenuItem> Top Categories    <BiCategory size='10%' /></MenuItem>
+					<MenuItem> Sign Out   <RxExit size='10%' /></MenuItem>
 				</Menu>
 			</Sidebar>
 			<main>
 				{!collapsed ?
-					<button onClick={() => collapseSidebar()} ><VscChromeClose size='4vh' style={{ color: 'black' }} /></button>
+					<button style={{ marginTop: '50vh' }} onClick={() => collapseSidebar()} ><VscChromeClose size='4vh' style={{ color: 'black' }} /></button>
 					:
 					<button onClick={() => collapseSidebar()} ><SlMenu size='4vh' style={{ color: 'black' }} /></button>}
 			</main>
